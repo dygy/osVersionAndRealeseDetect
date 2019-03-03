259407ee91a15c26c840657ba3cfdb4683729bd2
Dygy <Yukimurachris@gmail.com>
2019-03-03 17:09:54 +0300

    Добавьте файлы проекта.

---
+++ WindowsFormsApp2/Form1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Win32;
+
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            string releaseId = Registry.GetValue(@"HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion", "ReleaseId", "").ToString();
+            string drawString = "Версия ОС: " + Environment.OSVersion+" И версия релиза: "+ releaseId;
+            MessageBox.Show(drawString+" ");
+            InitializeComponent();
+        }
+     
+    }
+}
